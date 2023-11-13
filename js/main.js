ebb5c9ba2777c7c0f4c6fe9d44fa03c4c6ddf363
valentina80 <109566488+Lizpari@users.noreply.github.com>
2023-11-12 23:43:56 -0500

    musica

---
+++ js/main.js
@@ -0,0 +1,30 @@
+class musicPlayer {
+    constructor() {
+        this.play = this.play.bind(this);
+        this.playBtn = document.getElementById('play');
+        this.playBtn.addEventListener('click', this.play);
+        this.controlPanel = document.getElementById('control-panel');
+        this.status = false;
+    }
+
+    play() {
+        var audio = document.getElementById('Music'); 
+        let controlPanelObj = this.controlPanel,
+        infoBarObj = this.infoBar
+        Array.from(controlPanelObj.classList).find(function(element){   
+            return element !== "active" ? controlPanelObj.classList.add('active') : controlPanelObj.classList.remove('active');       
+        });
+
+
+        this.status = !this.status;
+
+        if(this.status == true){
+            audio.play(); 
+        }else{
+            audio.pause();
+        }
+        
+    }
+
+}
+const newMusicPlayer = new musicPlayer();
